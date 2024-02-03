22c45632446b509d2812708c867a877c69ccf619
chengdaosen <2206164816@qq.com>
2024-02-03 23:09:23 +0800

    修复底部导航栏选中图标不跳转问题

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,5 +1,6 @@
 Component({
   data: {
+    path: 'pages/add/index',
     selected: 0,
     color: '#6e6d6b',
     selectedColor: '#0061b0',
@@ -9,29 +10,27 @@ Component({
       {
         pagePath: '/pages/index/index',
         text: '首页',
+        iconPath: '../assets/icon/首页.png',
+        selectedIconPath: '../assets/icon/首页点击.png',
       },
       {
         pagePath: '/pages/my/index',
         text: '我的',
+        iconPath: '../assets/icon/我的.png',
+        selectedIconPath: '../assets/icon/我的点击.png',
       },
     ],
   },
   attached() {},
   methods: {
     switchTab(e) {
-      const data = e.currentTarget.dataset;
-      const url = data.path;
-      // 更新 list 数组中的 selected 字段
-      const newList = this.data.list.map((item, index) => {
-        return {
-          ...item,
-          selected: index === data.index,
-        };
-      });
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({ url })
       this.setData({
-        list: newList,
-      });
-      wx.switchTab({ url });
+        selected: data.index,
+      })
+      console.log('Selected value:', this.data.selected)
     },
   },
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,17 @@
-Component({
-  pageLifetimes: {
-    show() {
-      if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 0,
-        })
-      }
-    },
+Page({
+  data: {
+    value: '',
+  },
+  onSearch() {
+    console.log('搜索了')
+  },
+  // 页面显示时触发
+  onShow() {
+    // 在页面显示时修改底部导航栏状态
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 0,
+      })
+    }
   },
 })
